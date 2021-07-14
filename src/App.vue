d8849ac6ae838d9ad7cfb9a8afcdae3f8662c613
sanqi377 <sanqi377@outlook.com>
2021-07-14 14:53:40 +0800

    add upload components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Vue 常用组件</h1>
-    <Upload />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <h1>Vue 常用组件</h1>
+        <Upload />
+    </div>
 </template>
 
 <script>
-import Upload from '@/components/Upload/index.vue'
+import Upload from "@/components/Upload/index.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Upload
-  }
-}
+    name: "App",
+    components: {
+        Upload,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/Upload/index.vue
+++ src/components/Upload/index.vue
@@ -1,80 +1,63 @@
 <!-- 图片上传组件 -->
 <template>
-  <div class="upload">
-    <input
-      type="file"
-      accept="image/*"
-      ref="upload"
-      @change="getFileInfo"
-    /><span class="submit" @click="submitFile()">上传图片</span>
-  </div>
+    <div class="upload">
+        <input
+            type="file"
+            accept="image/*"
+            ref="upload"
+            @change="getFileInfo"
+        />
+        <span class="submit" @click="submitFile()">上传图片</span>
+    </div>
 </template>
 <script>
+import axios from "axios";
 var fileDom;
 export default {
-  name: "Upload",
-  methods: {
-    getFileInfo: (e) => {
-      fileDom = e.target;
+    name: "Upload",
+    methods: {
+        getFileInfo: (e) => {
+            fileDom = e.target;
+        },
+        submitFile: () => {
+            if (fileDom) {
+                let file = fileDom.files[0];
+                let formdata = new FormData(); //formdata格式
+                formdata.append("file", file); //图片文件
+                formdata.append("name", file.name); //其他参数
+                axios({
+                    method: "POST",
+                    data: formdata,
+                    url: "http://api.xqphp.com/admin/common/upload",
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }).then((response) => {
+                    console.log(response.data);
+                });
+            } else {
+                console.log(0);
+            }
+        },
     },
-    submitFile: () => {
-      let file = fileDom.files[0];
-      let formdata = new FormData(); //formdata格式
-      // formdata.append("picture", file); //图片文件
-      formdata.append("name", "1"); //其他参数
-      // formdata.append("introduce", this.product_add.introduce); //其他参数
-      console.log(formdata);
-      console.log(file);
-      // this.$refs.validate((valid) => {
-      //   //校验成功
-      //   if (valid) {
-      //     let myfile = this.$refs.upload;
-      //     let files = myfile.files;
-      //     let file = files[0];
-      //     let formdata = new FormData(); //formdata格式
-      //     formdata.append("picture", file); //图片文件
-      //     formdata.append("name", this.product_add.name); //其他参数
-      //     formdata.append("introduce", this.product_add.introduce); //其他参数
-      //     console.log(formdata);
-      //     //   // 发送请求
-      //     //   this.axios({
-      //     //     method: "POST",
-      //     //     data: formdata,
-      //     //     url: "/product_Description/product/addProduct",
-      //     //     headers: { "Content-Type": "multipart/form-data" },
-      //     //   }).then((response) => {
-      //     //     this.initProduct();
-      //     //     if (resp.data.code == 0) {
-      //     //       this.$message({
-      //     //         message: "添加产品成功！",
-      //     //         type: "success",
-      //     //       });
-      //     //     } else {
-      //     //       this.$message.error("添加产品失败！");
-      //     //     }
-      //     //   });
-      //   }
-      // });
-    },
-  },
 };
 </script>
 <style scoped>
 .upload {
-  display: inline-block;
-  border: 1px solid #000;
-  border-radius: 5px;
-  padding: 5px 8px;
+    display: inline-block;
+    border: 1px solid #000;
+    border-radius: 5px;
+    padding: 5px 8px;
 }
 
 .submit {
-  display: inline-block;
-  background-color: teal;
-  border: 1px solid #fff;
-  padding: 5px 8px;
-  color: #fff;
-  font-size: 12px;
-  border-radius: 5px;
-  cursor: pointer;
+    display: inline-block;
+    background-color: teal;
+    border: 1px solid #fff;
+    padding: 5px 8px;
+    color: #fff;
+    font-size: 12px;
+    border-radius: 5px;
+    cursor: pointer;
 }
 </style>
